7dcad3804d4d118447d609118fa707ebb9d70759
Rijad <rijad99.sm@gmail.com>
2025-04-08 20:03:56 +0200

    Fix dialog close button style

--- src/pages/components/countriest-list/components/country-details-dialog/CountryDetailsDialog.tsx
+++ src/pages/components/countriest-list/components/country-details-dialog/CountryDetailsDialog.tsx
@@ -1,10 +1,17 @@
 // Components
 import Button from "../../../../../components/button/Button";
-import { ButtonSize, ButtonType } from "../../../../../components/button/Button.types";
 import { Dialog } from "../../../../../components/dialog/Dialog";
+import Svg from "../../../../../components/svg/Svg";
+
+// Types
+import { ButtonSize, ButtonType } from "../../../../../components/button/Button.types";
+import { SvgColors, SvgStrokeLineCap, SvgStrokeLineJoin } from "../../../../../components/svg/Svg.types";
 
 // Styles
 import countryDetailsDialogStyle from './CountryDetailsDialog.module.scss';
+
+// Icons
+import { icons } from "../../../../../common/icons/icons";
 
 
 
@@ -17,6 +24,7 @@ function CountryDetailsDialog({ handleCloseCountryDetailsDialog }: CountryDetail
     return (
         <Dialog classes={countryDetailsDialogStyle.countryDetailsDialog}>
             <Button size={ButtonSize.MEDIUM} type={ButtonType.PRIMARY} onClick={handleCloseCountryDetailsDialog} additionalClasses={countryDetailsDialogStyle.closeCountryDetailsDialogButton}>
+                <Svg width='8' height='6' viewBox='0 0 12 7' path={icons.arrowIcon} additionalClasses={`${countryDetailsDialogStyle.arrowIcon} ${SvgColors.BLACK}`} stroke={SvgColors.BLACK} strokeWidth='1.5' strokeLinecap={SvgStrokeLineCap.ROUND} strokeLinejoin={SvgStrokeLineJoin.ROUND} />
                 Back
             </Button>
         </Dialog>
