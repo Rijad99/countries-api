e73adc666fe120d4981dc4dcb27f4bbcef267bbd
rsmajlov <rijad.smajlovic@dccs.eu>
2025-08-26 14:32:23 +0200

    Improve and finish SystemError component

--- src/components/system-error/SystemError.tsx
+++ src/components/system-error/SystemError.tsx
@@ -9,52 +9,77 @@ import Svg from '../svg/Svg.tsx';
 import Button from '../button/Button.tsx';
 
 // Types
-import { SvgColors } from '../svg/Svg.types.ts';
+import { SvgColors, SvgStrokeLineCap, SvgStrokeLineJoin } from '../svg/Svg.types.ts';
 import { ButtonSize, ButtonType } from '../button/Button.types.ts';
+import { QueryObserverResult } from '@tanstack/react-query';
 
 interface SystemErrorProps {
   id: string;
+  title: string,
   label: string;
   error: Error;
-  onShowErrorDetails: () => void;
+  onRefetchData: () => Promise<QueryObserverResult<QueryObserverResult, Error>>;
 }
 
-export function SystemError({ id, label, error, onShowErrorDetails }: SystemErrorProps) {
-  const renderShowErrorDetailsButton = onShowErrorDetails ? (
-    <Button
-      size={ButtonSize.SMALLEST}
-      type={ButtonType.PRIMARY}
-      additionalClasses={systemErrorStyles.systemErrorDetailsButton}
-      onClick={onShowErrorDetails}
-    >
-      <Svg
-        path={icons.arrowIcon}
-        strokeWidth={'2'}
-        width="21"
-        height="19"
-        viewBox="0 -6 20 21"
-        stroke={SvgColors.GRAY}
-      />
-    </Button>
-  ) : null;
-
+export function SystemError({ id, title, label, error, onRefetchData }: SystemErrorProps) {
   return (
     <div id={id} className={`${systemErrorStyles.systemError}`}>
-      <div className={systemErrorStyles.systemErrorContainer}>
-        <h5 className={systemErrorStyles.title}>System Error</h5>
-        <div className={systemErrorStyles.systemErrorInfo}>
-          <Svg
-            path={icons.errorIcon}
-            strokeWidth={'2'}
-            width="21"
-            height="19"
-            viewBox="0 -0.5 24 24"
-            stroke={SvgColors.RED}
-          />
-          <span className={systemErrorStyles.label}>{label}</span>
+      <div className={systemErrorStyles.systemErrorDetails}>
+        <div className={systemErrorStyles.systemErrorContainer}>
+          <h5 className={systemErrorStyles.title}>{title}</h5>
+          <div className={systemErrorStyles.systemErrorInfo}>
+            <Svg
+              path={icons.errorIcon}
+              strokeWidth={'2'}
+              width="21"
+              height="19"
+              viewBox="0 -0.5 24 24"
+              stroke={SvgColors.RED}
+            />
+            <span className={systemErrorStyles.label}>{label}</span>
+          </div>
         </div>
+        <TryAgainButton onRefetchData={onRefetchData} />
       </div>
-      {renderShowErrorDetailsButton}
+      <SystemErrorDetailsDescription error={error} />
+    </div>
+  );
+}
+
+interface TryAgainButtonProps {
+  onRefetchData: () => Promise<QueryObserverResult<QueryObserverResult, Error>>;
+}
+
+function TryAgainButton({ onRefetchData }: TryAgainButtonProps) {
+  return (
+    <Button
+      size={ButtonSize.MEDIUM}
+      type={ButtonType.PRIMARY}
+      additionalClasses={systemErrorStyles.systemErrorDetailsButton}
+      onClick={onRefetchData}>
+      <Svg
+        path={icons.refreshIcon}
+        strokeWidth={'2'}
+        width="28"
+        height="16"
+        viewBox="10 0 22 22"
+        stroke={SvgColors.GRAY}
+        strokeLinejoin={SvgStrokeLineJoin.ROUND}
+        strokeLinecap={SvgStrokeLineCap.ROUND}
+      />
+      Try Again
+    </Button>
+  );
+}
+
+interface SystemErrorDetailsDescriptionProps {
+  error: Error;
+}
+
+function SystemErrorDetailsDescription({ error }: SystemErrorDetailsDescriptionProps) {
+  return (
+    <div className={systemErrorStyles.systemErrorDetailsDescription}>
+      <span>{error.message}</span>
     </div>
   );
 }
